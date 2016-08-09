77b909264b53321b0f9d08717862bd4b47611573
Sorae <seokmin@nhnnext.org>
2016-08-09 22:25:23 +0900

    pch 설정 완료

--- sogalbi/Classes/AppDelegate.cpp
+++ sogalbi/Classes/AppDelegate.cpp
@@ -1,7 +1,6 @@
+#include "pch.h"
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
-
-USING_NS_CC;
 
 static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
