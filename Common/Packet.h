a380a28415bc91b35d15010e9f05a6a14605c860
Seokmin <seokmin@nhnnext.org>
2016-08-15 01:39:40 +0900

    update packets

--- Common/Packet.h
+++ Common/Packet.h
@@ -11,10 +11,34 @@ namespace PACKET
 
 	const auto MAX_USER_ID_LEN = 5;
 	const auto MAX_USER_PW_LEN = 10;
+	const auto AUTH_TOKEN_LEN = 20;
 	struct PaketLoginReq
 	{
 		wchar_t _id[MAX_USER_ID_LEN+1]; // +1은 nullptr을 위함
 		wchar_t _pw[MAX_USER_PW_LEN+1];
+		char	_authToken[AUTH_TOKEN_LEN + 1];
+	};
+	struct PacketLoginRes
+	{
 
+	};
+
+
+	struct PacketRoomListReq
+	{
+	};
+	
+	const auto NUM_OF_ROOM_IN_ONE_REQ = 6;
+	struct PacketRoomListRes
+	{
+		short	_pageNum;
+	};
+
+	const auto MAX_ROOM_CAPTION_LEN = 15;
+	struct RoomInfoSmall
+	{
+		short	_roomNum;
+		wchar_t _caption[MAX_ROOM_CAPTION_LEN+1];
+		short	_userCountInRoom;
 	};
 }
