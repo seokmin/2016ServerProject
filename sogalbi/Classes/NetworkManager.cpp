6ce0fc7efd2781a3975bf60aa491fb141ed900c5
Sorae <seokmin@nhnnext.org>
2016-08-24 01:33:49 +0900

    클라이언트 패킷받기 1차 완성
    테스트 못해봄, 빌드 됨. 테스트 후 버그 수정 계획

--- sogalbi/Classes/NetworkManager.cpp
+++ sogalbi/Classes/NetworkManager.cpp
@@ -30,17 +30,64 @@ void NetworkManager::initTcp()
 	}
 }
 
-COMMON::RecvPacketInfo NetworkManager::createPacketFromRecvBuffer()
+// 패킷 큐에서 딱 하나만 만들어서 리턴한다
+void NetworkManager::recvAndGivePacketsToCallbackFuncThread(std::function<void(const COMMON::PACKET_ID, const short, char*)> callbackFunc)
 {
-	auto receivedSize = recv(_sock, _recvBuffer, sizeof(_recvBuffer), 0);
-	auto remainingDataSizeInBuf = receivedSize;
-	
-	// 헤더를 읽을 수 있을 때
-	while (remainingDataSizeInBuf >= COMMON::PACKET_HEADER_SIZE)
-	{
+	using namespace COMMON;
 
+	// 버퍼에 남아있는 데이터의 양. 일관성을 지켜주려면 초기값은 당연히 0
+	auto remainingDataSizeInRecvBuffer = 0;
+
+	while (true)
+	{
+		// 소켓 연결이 끊겼으면 알아서 recv 시도를 그만둔다.
+		if (_sock <= 0)
+			break;
+		// 받을 수 있을 만큼 데이터를 받는다. 이때 채우기 시작하는 위치는 남아있던 데이터의 바로 뒤
+		auto receivedSize = recv(_sock, _recvBuffer + remainingDataSizeInRecvBuffer, sizeof(_recvBuffer), 0);
+		// 버퍼에 남아있는 데이터의 양. 일관성을 지켜주려면 recv한 만큼 늘려주는 것이 당연.
+		remainingDataSizeInRecvBuffer += receivedSize;
+		// 다음번에 만들 패킷 위치는 recv버퍼 맨 앞부터.
+		auto nextPacketAddress = _recvBuffer;
+
+		while (true)
+		{
+			// 헤더를 읽을 수 있을 때
+			if (remainingDataSizeInRecvBuffer >= PACKET_HEADER_SIZE)
+			{
+				// 헤더를 만든다.
+				auto packetHeader = (PacketHeader*)nextPacketAddress;
+				// 바디를 만들기에도 충분할 때
+				if (remainingDataSizeInRecvBuffer >= packetHeader->_bodySize + PACKET_HEADER_SIZE)
+				{
+					// 여기까지 왔다면 패킷을 만들 수 있는 것. 패킷을 부탁한 쪽에 넘긴다.
+					callbackFunc(packetHeader->_id, packetHeader->_bodySize, nextPacketAddress + PACKET_HEADER_SIZE);
+
+					// 다음에 만들 패킷의 주소는 지금 만든 패킷의 바로 뒤이다.
+					nextPacketAddress += PACKET_HEADER_SIZE + packetHeader->_bodySize;
+					// 또한, 하나 만들었으니 남은 데이터 양이 줄어들었다는 것을 표시한다.
+					remainingDataSizeInRecvBuffer -= PACKET_HEADER_SIZE + packetHeader->_bodySize;
+					continue; // 하나 만들었으니 다음 패킷을 만들러 출발한다.
+				}
+			}
+			// 여기 왔다는 것은 패킷을 만들기에 데이터가 모자라다는 것.
+			// 또한 기존 만들었던 패킷들을 callbackFunc()가 전부 소화했다는 것.
+			// 남은 재료를 recv버퍼의 가장 앞으로 당기고 다음번 Recv를 기다린다.
+			memcpy(_recvBuffer, nextPacketAddress, remainingDataSizeInRecvBuffer);
+			break;
+		}
+
+		// 만들 수 있을 만큼 다 만들었으므로 다음번 Recv를 기다린다.
 	}
-	return COMMON::RecvPacketInfo{};
+
+
+}
+
+void NetworkManager::setRecvCallback(std::function<void(const COMMON::PACKET_ID, const short, char*)> callbackFunc)
+{
+	using namespace COMMON;
+	// TODO : 짜는 중
+	std::thread(std::bind(&NetworkManager::recvAndGivePacketsToCallbackFuncThread,this,callbackFunc));
 }
 
 // send() 성공여부 반환
@@ -73,15 +120,6 @@ bool NetworkManager::sendPacket(const COMMON::PACKET_ID packetId, const short da
 	return true;
 }
 
-void NetworkManager::setRecvCallback(std::function<void(const COMMON::PACKET_ID, const short, char*)> callbackFunc)
-{
-	// TODO : 짜는 중
-	auto recvThread = [&]()
-	{
-		createPacketFromRecvBuffer();
-	};
-}
-
 NetworkManager* NetworkManager::getInstance()
 {
 	if (_instance == nullptr)
@@ -90,10 +128,11 @@ NetworkManager* NetworkManager::getInstance()
 }
 
 // 성공시 true, 실패시 false 반환
-bool NetworkManager::connectTcp(std::string serverIp, int serverPort)
+bool NetworkManager::connectTcp(std::string serverIp, int serverPort, std::function<void(const COMMON::PACKET_ID, const short, char*)> recvCallback)
 {
 	auto returnVal = true;
-	_mutex.lock();
+	if (_mutex.try_lock() == false)
+		return false;
 	SOCKADDR_IN serverAddr;
 	ZeroMemory(&serverAddr, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
@@ -107,6 +146,7 @@ bool NetworkManager::connectTcp(std::string serverIp, int serverPort)
 		ClientLogger::logThreadSafe("connect() failed");
 		returnVal = false;
 	}
+	setRecvCallback(recvCallback);
 	_mutex.unlock();
 	return returnVal;
 }
